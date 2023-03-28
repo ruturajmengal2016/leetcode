8b7053a25feeec0626a3879d897f83104fca1d88
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-28 11:22:46 +0530

    commit

--- valid.js
+++ valid.js
@@ -1,21 +1,23 @@
-// Valid Parentheses 
-function valid(str){
-    let array = []
-    const check ={
-        "(":")",
-        "{":"}",
-        "[":"]"
+// Valid Parentheses
+function valid(str) {
+  let array = []; // Empty array
+  const check = {
+    // Conditions object
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  for (let i = 0; i < str.length; i++) {
+    // iteration
+    if (str[i] == "(" || str[i] == "{" || str[i] == "[") {
+      // check condition of string 1st character
+      array.push(str[i]);
+    } else if (check[array.pop()] != str[i]) {
+      // return fasle if not match each other
+      return false;
     }
-    for (let i = 0; i < str.length; i++) {
-        if(str[i] == "(" || str[i] == "{" || str[i] == "["){
-            array.push(str[i])
-        }else if(check[array.pop()] != str[i]){
-            return false
-        }
-        
-    }
-    return array.length ? false : true
+  }
+  return array.length ? false : true; // return true if array length is 0
 }
 
-
-console.log(valid("{}[}"))
+console.log(valid("{}[}"));
